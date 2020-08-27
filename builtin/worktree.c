59a55c7d54eb33b233e70b78ddd51809b170897f
Eric Sunshine <sunshine@sunshineco.com>
2020-08-27 04:21:27 -0400

    worktree: teach "repair" to fix outgoing links to worktrees
    The .git/worktrees/<id>/gitdir file points at the location of a linked
    worktree's .git file. Its content must be of the form
    /path/to/worktree/.git (from which the location of the worktree itself
    can be derived by stripping the "/.git" suffix). If the gitdir file is
    deleted or becomes corrupted or outdated, then Git will be unable to
    find the linked worktree. An easy way for the gitdir file to become
    outdated is for the user to move the worktree manually (without using
    "git worktree move"). Although it is possible to manually update the
    gitdir file to reflect the new linked worktree location, doing so
    requires a level of knowledge about worktree internals beyond what a
    user should be expected to know offhand.
    
    Therefore, teach "git worktree repair" how to repair broken or outdated
    .git/worktrees/<id>/gitdir files automatically. (For this to work, the
    command must either be invoked from within the worktree whose gitdir
    file requires repair, or from within the main or any linked worktree by
    providing the path of the broken worktree as an argument to "git
    worktree repair".)
    
    Signed-off-by: Eric Sunshine <sunshine@sunshineco.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/worktree.c
+++ builtin/worktree.c
@@ -1042,15 +1042,18 @@ static void repair_cb(int iserr, const char *path, const char *msg, void *cb_dat
 
 static int repair(int ac, const char **av, const char *prefix)
 {
+	const char **p;
+	const char *self[] = { ".", NULL };
 	struct option options[] = {
 		OPT_END()
 	};
 	int rc = 0;
 
 	ac = parse_options(ac, av, prefix, options, worktree_usage, 0);
-	if (ac)
-		usage_with_options(worktree_usage, options);
 	repair_worktrees(repair_cb, &rc);
+	p = ac > 0 ? av : self;
+	for (; *p; p++)
+		repair_worktree_at_path(*p, repair_cb, &rc);
 	return rc;
 }
 
